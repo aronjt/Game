3103e2dd615ca2601bbb1a8e6f57c6d8514603ff
aronjt <jtelekiaron@gmail.com>
2020-12-28 19:24:06 +0100

    newcar added

--- src/org/progmatic/Car.java
+++ src/org/progmatic/Car.java
@@ -10,7 +10,7 @@ public class Car extends JComponent {
         g.setColor(Color.blue);
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(4));
-        String imageURL = "car.jpg";
+        String imageURL = "car.png";
         Image image = Toolkit.getDefaultToolkit().getImage(imageURL);
         g2.drawImage(image, 5, 5, this);
     }
