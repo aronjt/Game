765009d64b229597a648b4c215f87bc42c1c021d
aronjt <jtelekiaron@gmail.com>
2020-12-28 18:35:55 +0100

    panel

--- src/org/progmatic/Main.java
+++ src/org/progmatic/Main.java
@@ -1,8 +1,19 @@
 package org.progmatic;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JFrame frame = new JFrame();
+
+        frame.setBounds(10, 10, 500, 800);
+        frame.setTitle("RaceCar");
+        frame.setBackground(Color.black);
+        frame.setResizable(false);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
